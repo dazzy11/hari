b559874b63888adc4cdf3b5ec450f4eb088551f4
Deepak madhukumar <deepakmadhukumar1124@gmail.com>
2024-02-27 16:04:30 +0530

    client.js

---
+++ routes/messages.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const Message = require('../models/Message');
+
+// GET all messages
+router.get('/', async (req, res) => {
+  try {
+    const messages = await Message.find().sort({ timestamp: 1 });
+    res.json(messages);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// POST a new message
+router.post('/', async (req, res) => {
+  const { text, sender } = req.body;
+
+  try {
+    const newMessage = new Message({
+      text,
+      sender
+    });
+
+    const message = await newMessage.save();
+    res.json(message);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
